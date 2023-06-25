155c5eea67d66baa442c95963cbecb9dc429d3c7
lanroo <yladacs@gmail.com>
2023-06-25 18:49:18 -0300

    navbar created

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,14 +1,34 @@
+import './styles.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 function Navbar() {
   return (
-    <nav className="bg-primary">
-      <div>
-        <a href="link">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
           <h4>Catalog</h4>
         </a>
-        <div>
-          <ul>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscatalog-navbar"
+          aria-controls="dscatalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="dscatalog-navbar" >
+          <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">HOME</a>
+              <a href="link" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
